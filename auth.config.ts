910fb7d1c6c5d6e450981ae62441af0748ac2b8c
kenne835 <kennedygb12@gmail.com>
2024-01-30 21:04:02 -0600

    test

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,12 @@
 import type { NextAuthConfig } from 'next-auth';
- 
+
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
+      console.log('PATH', nextUrl.pathname)
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
